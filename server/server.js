083987f5a55b7485d09484c7254cb4c5d892388a
carlos_daniell <carlosdaniell.tecpuhawan@cgi.com>
2024-06-10 15:16:15 +0200

    mongodbGuide

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const wofDataRoutes = require('./routes/wofDataRoute') //Routes
 
 //express app
